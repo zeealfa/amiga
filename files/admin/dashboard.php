659d239d3c8e8e8942efabb8df630f6a0dd41f1a
Zee <zee.alfa@gmail.com>
2026-07-08 14:52:11 +0700

    Phase 03a: add dashboard stub

---
+++ files/admin/dashboard.php
@@ -0,0 +1,52 @@
+<?php
+if (!isset($_SESSION)) {
+    session_start();
+}
+require_once __DIR__ . '/_auth.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>AmigaSource.com - Dashboard</title>
+<link rel="stylesheet" href="../style.css">
+</head>
+<body class="bg-dddddd">
+
+<?php require __DIR__ . '/_header.php'; ?>
+
+<br>
+
+<center>
+<table width="70%" align="center" cellpadding="0" cellspacing="0">
+<tr>
+	<td width="22%" valign="top" class="bg-bbbbbb">
+		<?php require __DIR__ . '/_nav.php'; ?>
+	</td>
+	<td width="3%"></td>
+	<td width="75%" valign="top">
+		<table width="100%" cellpadding="1" cellspacing="0" class="bg-637b94">
+			<tr><td>
+				<table width="100%" cellpadding="1" cellspacing="1" class="bg-fff">
+					<tr>
+						<td align="center" class="bg-ff2626">
+							<span class="txt-4-fff"><b>WELCOME, <?php echo strtoupper(htmlspecialchars($_SESSION['username'])); ?></b></span>
+						</td>
+					</tr>
+					<tr>
+						<td class="bg-f4f4f4" style="padding:12px;">
+							<span class="txt-2-000">You are logged in. Full dashboard content ships in Phase 03b.</span>
+						</td>
+					</tr>
+				</table>
+			</td></tr>
+		</table>
+		<br><br>
+	</td>
+</tr>
+</table>
+</center>
+
+<?php require __DIR__ . '/_footer.php'; ?>
+
+</body>
+</html>
