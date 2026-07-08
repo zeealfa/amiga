0dea7ae62531dced4ac9b17ebf5b6ab3bd15b801
Zee <zee.alfa@gmail.com>
2026-07-08 15:48:16 +0700

    Add Back to Site link and home-linking logo to admin header

--- files/admin/_header.php
+++ files/admin/_header.php
@@ -2,13 +2,13 @@
 	<tr>
 		<td>
 			<tr>
-				<a href="dashboard.php">
+				<a href="../index.php">
 				<img src="../web_images/static/lg-asbnr.jpg" alt="Amiga Source" height="120" width="380">
 				</a>
 			</tr>
 			<tr>
 				<td align="right" class="bg-ff9900" cellpadding="16" cellspacing="8">
-					<span class="txt-3"><b>Logged in as: <u><?php echo htmlspecialchars($_SESSION['username']); ?></u> (<?php echo htmlspecialchars($_SESSION['role']); ?>) &nbsp; | &nbsp; <a href="logout.php">Log Out</a></b></span>
+					<span class="txt-3"><b>Logged in as: <u><?php echo htmlspecialchars($_SESSION['username']); ?></u> (<?php echo htmlspecialchars($_SESSION['role']); ?>) &nbsp; | &nbsp; <a href="../index.php">Back to Site</a> &nbsp; | &nbsp; <a href="logout.php">Log Out</a></b></span>
 				</td>
 			</tr>
 		</td>
