7e4fb6cab1fc71daebfdd33f063d37205befbeaa
Zee <zee.alfa@gmail.com>
2026-07-10 15:45:14 +0700

    Add legacy bgcolor/font attributes to sidebar_add_link.php

--- files/sidebar_add_link.php
+++ files/sidebar_add_link.php
@@ -1,32 +1,32 @@
-<table cellpadding="1" cellspacing="0" width="100%"  class="bg-slateblue">
+<table cellpadding="1" cellspacing="0" width="100%"  class="bg-slateblue" bgcolor="<?php echo bg_hex('slateblue'); ?>">
 	<tr>
 		<td>
-			<table width="100%" cellpadding="1"  cellspacing="0" class="bg-white">
+			<table width="100%" cellpadding="1"  cellspacing="0" class="bg-white" bgcolor="<?php echo bg_hex('white'); ?>">
 				<tr>
 					<td>
 						<table width="100%"  cellspacing="0" cellpadding="3">
 							<tr>
-								<td align="left" valign="top" class="bg-cyan">
-									<span class="txt-4-white">
+								<td align="left" valign="top" class="bg-cyan" bgcolor="<?php echo bg_hex('cyan'); ?>">
+									<font class="txt-4-white" face="Verdana, sans-serif" size="4" color="<?php echo txt_hex('white'); ?>">
 										<b>Add A Link</b><br>
-									</span>
+									</font>
 								</td>
 							</tr>
 						</table>
 
 						<table width="100%"  cellspacing="0" cellpadding="4">
 							<tr>
-								<td align="center" valign="top" class="bg-white">
-									<span class="txt-0-black">
+								<td align="center" valign="top" class="bg-white" bgcolor="<?php echo bg_hex('white'); ?>">
+									<font class="txt-0-black" face="Verdana, sans-serif" size="0" color="<?php echo txt_hex('black'); ?>">
 										Add a link {under construction}<br><br>
-									</span>
+									</font>
 								</td>
 							</tr>
 						</table>
 					</td>
 				</tr>
 			</table>
-		</td>	
+		</td>
 	</tr>
 </table>
 <br> 
