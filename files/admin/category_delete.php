4a5ff5096f1d480c9322a4bcc983584fe805bc03
Zee <zee.alfa@gmail.com>
2026-07-10 16:01:17 +0700

    Add legacy_colors.php attributes to admin/category_delete.php

--- files/admin/category_delete.php
+++ files/admin/category_delete.php
@@ -46,7 +46,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['confirm_delete']) && 
 <?php include_once __DIR__ . '/../legacy_colors.php'; ?>
 <style><?php include __DIR__ . '/../style.css'; ?></style>
 </head>
-<body class="bg-lightgray">
+<body class="bg-lightgray" bgcolor="<?php echo bg_hex('lightgray'); ?>">
 
 <?php require __DIR__ . '/_header.php'; ?>
 
@@ -55,25 +55,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['confirm_delete']) && 
 <center>
 <table width="60%" align="center" cellpadding="0" cellspacing="0">
 <tr>
-	<td width="25%" valign="top" class="bg-gray">
+	<td width="25%" valign="top" class="bg-gray" bgcolor="<?php echo bg_hex('gray'); ?>">
 		<?php require __DIR__ . '/_nav.php'; ?>
 	</td>
 	<td width="3%"></td>
 	<td width="72%" valign="top">
-		<table width="100%" cellpadding="1" cellspacing="0" class="bg-slateblue">
+		<table width="100%" cellpadding="1" cellspacing="0" class="bg-slateblue" bgcolor="<?php echo bg_hex('slateblue'); ?>">
 			<tr><td>
-				<table width="100%" cellpadding="1" cellspacing="1" class="bg-white">
+				<table width="100%" cellpadding="1" cellspacing="1" class="bg-white" bgcolor="<?php echo bg_hex('white'); ?>">
 					<tr>
-						<td align="center" class="bg-red">
-							<span class="txt-4-white"><b>DELETE CATEGORY</b></span>
+						<td align="center" class="bg-red" bgcolor="<?php echo bg_hex('red'); ?>">
+							<font class="txt-4-white" face="Verdana, sans-serif" size="4" color="<?php echo txt_hex('white'); ?>"><b>DELETE CATEGORY</b></font>
 						</td>
 					</tr>
 <?php if ($child_count > 0): ?>
 					<tr>
-						<td class="bg-whitesmoke" style="padding:16px;">
-							<span class="txt-2-black">
+						<td class="bg-whitesmoke" bgcolor="<?php echo bg_hex('whitesmoke'); ?>" style="padding:16px;">
+							<font class="txt-2-black" face="Verdana, sans-serif" size="2" color="<?php echo txt_hex('black'); ?>">
 								Cannot delete <b><?php echo htmlspecialchars($category['title']); ?></b>: remove or move its <?php echo (int) $child_count; ?> subcategories first.
-							</span>
+							</font>
 							<br><br>
 							<center>
 								<a href="categories.php" class="bg-gray" style="padding:4px 20px; font-weight:bold; text-decoration:none;">Back</a>
@@ -82,10 +82,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['confirm_delete']) && 
 					</tr>
 <?php else: ?>
 					<tr>
-						<td class="bg-whitesmoke" style="padding:16px;">
-							<span class="txt-2-black">
+						<td class="bg-whitesmoke" bgcolor="<?php echo bg_hex('whitesmoke'); ?>" style="padding:16px;">
+							<font class="txt-2-black" face="Verdana, sans-serif" size="2" color="<?php echo txt_hex('black'); ?>">
 								Are you sure you want to delete <b><?php echo htmlspecialchars($category['title']); ?></b>?
-							</span>
+							</font>
 							<br><br>
 							<center>
 								<form method="post" action="category_delete.php" style="display:inline;">
