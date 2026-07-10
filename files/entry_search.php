c2ca1bb24538f87fd3fa8aee5a3ea38b5bf11512
Zee <zee.alfa@gmail.com>
2026-07-10 15:44:13 +0700

    Add legacy bgcolor/font attributes to content_search_proc.php and entry_search.php

--- files/entry_search.php
+++ files/entry_search.php
@@ -3,17 +3,18 @@
 		session_start();
 	}
 	$_SESSION["content_type"]='search';
+	include_once __DIR__ . '/legacy_colors.php';
 ?>
 <table align=center cellpadding=2 cellspacing=0 border=0 width=100%>
-	<td class="bg-white" align=center colspan=3>
-		<span class="txt-4-black">
+	<td class="bg-white" bgcolor="<?php echo bg_hex('white'); ?>" align=center colspan=3>
+		<font class="txt-4-black" face="Verdana, sans-serif" size="4" color="<?php echo txt_hex('black'); ?>">
 			<?php
 				$search_r=$_POST['search'];
 				$search_f=$search_r;
 				include ("login_db.php");
 				include ("page_builder.php");
 			?>
-		</span>
+		</font>
 	</td>
 </table>
 <?php echo "<title>AmigaSource.com Search - ".$search_r."</title>"; ?>
