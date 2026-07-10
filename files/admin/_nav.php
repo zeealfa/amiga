ce9063abdaa54542472455fb50718ae9ff295fe0
Zee <zee.alfa@gmail.com>
2026-07-10 22:44:09 +0700

    Add admin submission review queue (submissions.php)

--- files/admin/_nav.php
+++ files/admin/_nav.php
@@ -2,6 +2,7 @@
 	<tr><td class="bg-slateblue" bgcolor="<?php echo bg_hex('slateblue'); ?>"><font class="txt-3-white" face="Verdana, sans-serif" size="3" color="<?php echo txt_hex('white'); ?>"><b><?php echo $_SESSION['role'] === 'admin' ? 'ADMIN MENU' : 'MY MENU'; ?></b></font></td></tr>
 	<tr><td class="bg-white" bgcolor="<?php echo bg_hex('white'); ?>"><font class="txt-2" face="Verdana, sans-serif" size="2"><b>&raquo; Dashboard</b></font></td></tr>
 <?php if ($_SESSION['role'] === 'admin'): ?>
+	<tr><td class="bg-white" bgcolor="<?php echo bg_hex('white'); ?>"><font class="txt-2" face="Verdana, sans-serif" size="2">&raquo; <a href="submissions.php">Submissions</a></font></td></tr>
 	<tr><td class="bg-white" bgcolor="<?php echo bg_hex('white'); ?>"><font class="txt-2" face="Verdana, sans-serif" size="2">&raquo; <a href="users.php">Users</a></font></td></tr>
 	<tr><td class="bg-white" bgcolor="<?php echo bg_hex('white'); ?>"><font class="txt-2" face="Verdana, sans-serif" size="2">&raquo; <a href="news.php">News</a></font></td></tr>
 	<tr><td class="bg-white" bgcolor="<?php echo bg_hex('white'); ?>"><font class="txt-2" face="Verdana, sans-serif" size="2">&raquo; <a href="links.php">Links</a></font></td></tr>
---
+++ files/admin/submissions.php
@@ -0,0 +1,106 @@
+<?php
+if (!isset($_SESSION)) {
+    session_start();
+}
+require_once __DIR__ . '/_auth.php';
+require_admin();
+
+$result = mysqli_query(
+    $myConnection,
+    "SELECT s.*, u.username FROM t_submissions s
+     JOIN t_users u ON u.id = s.submitted_by
+     WHERE s.status = 'pending'
+     ORDER BY s.created_at ASC"
+);
+$submissions = [];
+while ($row = mysqli_fetch_assoc($result)) {
+    $submissions[] = $row;
+}
+
+function submission_title($submission)
+{
+    if ($submission['type'] === 'link') {
+        return htmlspecialchars($submission['links_name']);
+    }
+    return htmlspecialchars($submission['news_date']) . ' &mdash; ' . htmlspecialchars(mb_substr(strip_tags($submission['news_story']), 0, 60)) . '&hellip;';
+}
+
+$flash = $_SESSION['flash_message'] ?? null;
+unset($_SESSION['flash_message']);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>AmigaSource.com - Submission Review Queue</title>
+<?php include_once __DIR__ . '/../legacy_colors.php'; ?>
+<style><?php include __DIR__ . '/../style.css'; ?></style>
+</head>
+<body class="bg-lightgray" bgcolor="<?php echo bg_hex('lightgray'); ?>">
+
+<?php require __DIR__ . '/_header.php'; ?>
+
+<br>
+
+<center>
+<table width="80%" align="center" cellpadding="0" cellspacing="0">
+<tr>
+	<td width="18%" valign="top" class="bg-gray" bgcolor="<?php echo bg_hex('gray'); ?>">
+		<?php require __DIR__ . '/_nav.php'; ?>
+	</td>
+	<td width="3%"></td>
+	<td width="79%" valign="top">
+		<table width="100%" cellpadding="1" cellspacing="0" class="bg-slateblue" bgcolor="<?php echo bg_hex('slateblue'); ?>">
+			<tr><td>
+				<table width="100%" cellpadding="1" cellspacing="1" class="bg-white" bgcolor="<?php echo bg_hex('white'); ?>">
+					<tr>
+						<td align="center" class="bg-red" bgcolor="<?php echo bg_hex('red'); ?>">
+							<font class="txt-4-white" face="Verdana, sans-serif" size="4" color="<?php echo txt_hex('white'); ?>"><b>SUBMISSION REVIEW QUEUE</b></font>
+						</td>
+					</tr>
+<?php if ($flash): ?>
+					<tr>
+						<td class="bg-orange" bgcolor="<?php echo bg_hex('orange'); ?>" style="padding:8px;">
+							<font class="txt-2-black" face="Verdana, sans-serif" size="2" color="<?php echo txt_hex('black'); ?>"><?php echo htmlspecialchars($flash); ?></font>
+						</td>
+					</tr>
+<?php endif; ?>
+					<tr>
+						<td class="bg-white" bgcolor="<?php echo bg_hex('white'); ?>" style="padding:8px;">
+							<table width="100%" cellpadding="4" cellspacing="0">
+								<tr class="bg-gray" bgcolor="<?php echo bg_hex('gray'); ?>">
+									<td><font class="txt-2-black" face="Verdana, sans-serif" size="2" color="<?php echo txt_hex('black'); ?>"><b>Type</b></font></td>
+									<td><font class="txt-2-black" face="Verdana, sans-serif" size="2" color="<?php echo txt_hex('black'); ?>"><b>Action</b></font></td>
+									<td><font class="txt-2-black" face="Verdana, sans-serif" size="2" color="<?php echo txt_hex('black'); ?>"><b>Submitted By</b></font></td>
+									<td><font class="txt-2-black" face="Verdana, sans-serif" size="2" color="<?php echo txt_hex('black'); ?>"><b>Item</b></font></td>
+									<td><font class="txt-2-black" face="Verdana, sans-serif" size="2" color="<?php echo txt_hex('black'); ?>"><b>Submitted</b></font></td>
+									<td><font class="txt-2-black" face="Verdana, sans-serif" size="2" color="<?php echo txt_hex('black'); ?>"><b>Actions</b></font></td>
+								</tr>
+<?php if (empty($submissions)): ?>
+								<tr><td colspan="6"><font class="txt-1" face="Verdana, sans-serif" size="1">No pending submissions.</font></td></tr>
+<?php endif; ?>
+<?php foreach ($submissions as $submission): ?>
+								<tr>
+									<td><font class="txt-2-black" face="Verdana, sans-serif" size="2" color="<?php echo txt_hex('black'); ?>"><?php echo htmlspecialchars(ucfirst($submission['type'])); ?></font></td>
+									<td><font class="txt-1" face="Verdana, sans-serif" size="1"><?php echo htmlspecialchars(ucfirst($submission['action'])); ?></font></td>
+									<td><font class="txt-1" face="Verdana, sans-serif" size="1"><?php echo htmlspecialchars($submission['username']); ?></font></td>
+									<td><font class="txt-1" face="Verdana, sans-serif" size="1"><?php echo submission_title($submission); ?></font></td>
+									<td><font class="txt-1" face="Verdana, sans-serif" size="1"><?php echo htmlspecialchars($submission['created_at']); ?></font></td>
+									<td><font class="txt-1" face="Verdana, sans-serif" size="1"><a href="submission_review.php?id=<?php echo (int) $submission['id']; ?>">Review</a></font></td>
+								</tr>
+<?php endforeach; ?>
+							</table>
+						</td>
+					</tr>
+				</table>
+			</td></tr>
+		</table>
+		<br><br>
+	</td>
+</tr>
+</table>
+</center>
+
+<?php require __DIR__ . '/_footer.php'; ?>
+
+</body>
+</html>
