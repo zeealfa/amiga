731d371a7a2a464c14aa0e0839c3c66df28e2c46
Zee <zee.alfa@gmail.com>
2026-07-10 09:04:55 +0700

    Redirect to a forced password change when must_change_password is set

--- files/admin/_auth.php
+++ files/admin/_auth.php
@@ -6,3 +6,9 @@ require_once __DIR__ . '/../includes/auth.php';
 require_once __DIR__ . '/../includes/db.php';
 
 require_login();
+
+$current_script = basename($_SERVER['SCRIPT_NAME']);
+if (!empty($_SESSION['must_change_password']) && $current_script !== 'force_password_change.php') {
+    header('Location: force_password_change.php');
+    exit;
+}
