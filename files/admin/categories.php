ac1028780565c577cecedf2448e12feaace56881
Zee <zee.alfa@gmail.com>
2026-07-10 16:00:58 +0700

    Add legacy_colors.php attributes to admin/categories.php

--- files/admin/categories.php
+++ files/admin/categories.php
@@ -50,11 +50,11 @@ function render_admin_tree_rows($nodes, $depth)
 {
     foreach ($nodes as $node) {
         $indent = str_repeat('&nbsp;&nbsp;&nbsp;&nbsp;', $depth);
-        echo '<tr><td><span class="txt-2-black">' . $indent . htmlspecialchars($node['title']);
+        echo '<tr><td><font class="txt-2-black" face="Verdana, sans-serif" size="2" color="' . txt_hex('black') . '">' . $indent . htmlspecialchars($node['title']);
         if (!$node['active']) {
             echo ' <i>(inactive)</i>';
         }
-        echo '</span></td><td>';
+        echo '</font></td><td>';
         if (!$node['is_first']) {
             echo '<form method="post" action="category_move.php" style="display:inline;">'
                 . '<input type="hidden" name="id" value="' . $node['id'] . '">'
@@ -94,7 +94,7 @@ $tree = build_admin_tree(fetch_categories_flat($myConnection));
 <?php include_once __DIR__ . '/../legacy_colors.php'; ?>
 <style><?php include __DIR__ . '/../style.css'; ?></style>
 </head>
-<body class="bg-lightgray">
+<body class="bg-lightgray" bgcolor="<?php echo bg_hex('lightgray'); ?>">
 
 <?php require __DIR__ . '/_header.php'; ?>
 
@@ -103,38 +103,38 @@ $tree = build_admin_tree(fetch_categories_flat($myConnection));
 <center>
 <table width="80%" align="center" cellpadding="0" cellspacing="0">
 <tr>
-	<td width="18%" valign="top" class="bg-gray">
+	<td width="18%" valign="top" class="bg-gray" bgcolor="<?php echo bg_hex('gray'); ?>">
 		<?php require __DIR__ . '/_nav.php'; ?>
 	</td>
 	<td width="3%"></td>
 	<td width="79%" valign="top">
-		<table width="100%" cellpadding="1" cellspacing="0" class="bg-slateblue">
+		<table width="100%" cellpadding="1" cellspacing="0" class="bg-slateblue" bgcolor="<?php echo bg_hex('slateblue'); ?>">
 			<tr><td>
-				<table width="100%" cellpadding="1" cellspacing="1" class="bg-white">
+				<table width="100%" cellpadding="1" cellspacing="1" class="bg-white" bgcolor="<?php echo bg_hex('white'); ?>">
 					<tr>
-						<td align="center" class="bg-red">
-							<span class="txt-4-white"><b>MANAGE CATEGORIES</b></span>
+						<td align="center" class="bg-red" bgcolor="<?php echo bg_hex('red'); ?>">
+							<font class="txt-4-white" face="Verdana, sans-serif" size="4" color="<?php echo txt_hex('white'); ?>"><b>MANAGE CATEGORIES</b></font>
 						</td>
 					</tr>
 <?php if ($flash): ?>
 					<tr>
-						<td class="bg-orange" style="padding:8px;">
-							<span class="txt-2-black"><?php echo htmlspecialchars($flash); ?></span>
+						<td class="bg-orange" bgcolor="<?php echo bg_hex('orange'); ?>" style="padding:8px;">
+							<font class="txt-2-black" face="Verdana, sans-serif" size="2" color="<?php echo txt_hex('black'); ?>"><?php echo htmlspecialchars($flash); ?></font>
 						</td>
 					</tr>
 <?php endif; ?>
 					<tr>
-						<td class="bg-whitesmoke" style="padding:8px;">
+						<td class="bg-whitesmoke" bgcolor="<?php echo bg_hex('whitesmoke'); ?>" style="padding:8px;">
 							<a href="category_form.php" class="bg-green" style="color:#ffffff; font-weight:bold; padding:4px 10px; text-decoration:none;">+ Add Root Category</a>
 						</td>
 					</tr>
 					<tr>
-						<td class="bg-white" style="padding:8px;">
+						<td class="bg-white" bgcolor="<?php echo bg_hex('white'); ?>" style="padding:8px;">
 							<table width="100%" cellpadding="4" cellspacing="0">
-								<tr class="bg-gray">
-									<td><span class="txt-2-black"><b>Title</b></span></td>
-									<td><span class="txt-2-black"><b>Reorder</b></span></td>
-									<td><span class="txt-2-black"><b>Actions</b></span></td>
+								<tr class="bg-gray" bgcolor="<?php echo bg_hex('gray'); ?>">
+									<td><font class="txt-2-black" face="Verdana, sans-serif" size="2" color="<?php echo txt_hex('black'); ?>"><b>Title</b></font></td>
+									<td><font class="txt-2-black" face="Verdana, sans-serif" size="2" color="<?php echo txt_hex('black'); ?>"><b>Reorder</b></font></td>
+									<td><font class="txt-2-black" face="Verdana, sans-serif" size="2" color="<?php echo txt_hex('black'); ?>"><b>Actions</b></font></td>
 								</tr>
 <?php render_admin_tree_rows($tree, 0); ?>
 							</table>
