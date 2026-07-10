553c3d9881ce7b4029adf7cc4d7635a740a743ba
Zee <zee.alfa@gmail.com>
2026-07-10 15:49:49 +0700

    Add legacy bgcolor/font attributes to sidebar_search.php

--- files/sidebar_search.php
+++ files/sidebar_search.php
@@ -1,31 +1,31 @@
-<table cellpadding="1" cellspacing="0" width="100%"  class="bg-slateblue">
+<table cellpadding="1" cellspacing="0" width="100%"  class="bg-slateblue" bgcolor="<?php echo bg_hex('slateblue'); ?>">
 	<tr>
 		<td>
-			<table width="100%" cellpadding="1"  cellspacing="0" class="bg-white">
+			<table width="100%" cellpadding="1"  cellspacing="0" class="bg-white" bgcolor="<?php echo bg_hex('white'); ?>">
 				<tr>
 					<td>
 						<table width="100%"  cellspacing="0" cellpadding="3">
 							<tr>
-								<td align="left" valign="top" class="bg-darkred">
-									<span class="txt-4-white">
+								<td align="left" valign="top" class="bg-darkred" bgcolor="<?php echo bg_hex('darkred'); ?>">
+									<font class="txt-4-white" face="Verdana, sans-serif" size="4" color="<?php echo txt_hex('white'); ?>">
 										<b>Search</b><br>
-									</span>
+									</font>
 								</td>
 							</tr>
 						</table>
 
 						<table width="100%"  cellspacing="0" cellpadding="4">
 							<tr>
-								<td align="center" valign="top" class="bg-white">
+								<td align="center" valign="top" class="bg-white" bgcolor="<?php echo bg_hex('white'); ?>">
 
 
 									<form action="/entry_search.php" method="post">
 										<input type="text" name="search" size=25 maxlength=125>
 										<p>
-										<span class="txt-0-black">
+										<font class="txt-0-black" face="Verdana, sans-serif" size="0" color="<?php echo txt_hex('black'); ?>">
 											Advanced Search (coming soon)<br><br>
 										<input type="submit">
-									</span></form>	<br>
+									</font></form>	<br>
 
 
 								</td>		
