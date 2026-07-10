56bd2592bef10554bcd3a08e6d381d33a57d5591
Zee <zee.alfa@gmail.com>
2026-07-10 15:36:23 +0700

    Add legacy bgcolor/font attributes to mod_footer.php

--- files/mod_footer.php
+++ files/mod_footer.php
@@ -1,18 +1,18 @@
 <!-- Footer starts here -->
 <table width="100%">
 	<tr>
-		<td align="center" class="bg-lightgray" width="100%">
-			<span class="txt-2">
+		<td align="center" class="bg-lightgray" bgcolor="<?php echo bg_hex('lightgray'); ?>" width="100%">
+			<font class="txt-2" face="Verdana, sans-serif" size="2">
 				<br>
 					<center>
 						<b> You can contact me at <a href="mailto:webmaster@amigasource.com">webmaster@amigasource.com</a></b>
 							<br><br>
 						Logo Copyright 2001.
 							<br>
-						Amiga is a registered trademark. <br><br><center><span class="txt-0">Site Copyright 2026, <br> Scott A. Pistorino<br>
-					</span></center>
+						Amiga is a registered trademark. <br><br><center><font class="txt-0" face="Verdana, sans-serif" size="0">Site Copyright 2026, <br> Scott A. Pistorino<br>
+					</font></center>
 				<br>
-			</span>
+			</font>
 		</td>
 	</tr>
 </table>
