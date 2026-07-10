1ca431de6e0b63ba46a5a8108deda85d4841d453
Zee <zee.alfa@gmail.com>
2026-07-10 15:38:01 +0700

    Add legacy bgcolor attributes to sec_body.php

--- files/sec_body.php
+++ files/sec_body.php
@@ -1,21 +1,21 @@
 
-<table class="bg-white" width="100%" align="center" cellpadding="0" cellspacing="0">
+<table class="bg-white" bgcolor="<?php echo bg_hex('white'); ?>" width="100%" align="center" cellpadding="0" cellspacing="0">
  	<tr>
 		<center>
 			<td width="70%">
-				<table class="bg-white" width="100%" align="center" cellpadding="6">
+				<table class="bg-white" bgcolor="<?php echo bg_hex('white'); ?>" width="100%" align="center" cellpadding="6">
 					<tr>
 						<!----width of sidebar---->
-						<td width="21%" valign="top" class="bg-gray">
+						<td width="21%" valign="top" class="bg-gray" bgcolor="<?php echo bg_hex('gray'); ?>">
 								<?php include 'mod_sidebar_chooser.php'; ?>
 						</td>
 					</tr>
 				</table>
 		</center>
 		<center>
-			<td valign="top" class="bg-white">
+			<td valign="top" class="bg-white" bgcolor="<?php echo bg_hex('white'); ?>">
 				<!----width of main content---->
-				<table class="bg-white" width="80%" align="center" cellpadding="0">
+				<table class="bg-white" bgcolor="<?php echo bg_hex('white'); ?>" width="80%" align="center" cellpadding="0">
 					<tr>
 						<td>
 								<?php if ($_SESSION["content_type"]=='news'){ include 'content_news.php'; } 
